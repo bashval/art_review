81498e1f7bf4903324337f899cbaf88a777e534a
Sanzhar <sanzhar162003@gmail.com>
2024-09-21 02:52:24 +0500

    Add verbose names to reviews/models.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -10,22 +10,29 @@ class Title(models.Model):
 
 
 class Review(models.Model):
-    text = models.TextField()
+    '''Модель отзывов.'''
+
+    text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField('Дата публикаций', auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='reviews'
+        User, on_delete=models.CASCADE,
+        related_name='reviews', verbose_name='Автор'
     )
     score = models.IntegerField(
         validators=[
             MinValueValidator(1, message='Оценка не должна быть меньше 1'),
             MaxValueValidator(10, message='Оценка не должна быть больше 10')
-        ]
+        ],
+        verbose_name='Оценка'
     )
     title_id = models.ForeignKey(
-        Title, on_delete=models.CASCADE, related_name='reviews'
+        Title, on_delete=models.CASCADE,
+        related_name='reviews', verbose_name='Произведение'
     )
 
     class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
         constraints = (
             models.UniqueConstraint(
                 fields=('author', 'title_id'),
@@ -35,11 +42,19 @@ class Review(models.Model):
 
 
 class Comment(models.Model):
-    text = models.TextField()
+    '''Модель комментариев.'''
+
+    text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField('Дата комментария', auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments'
+        User, on_delete=models.CASCADE,
+        related_name='comments', verbose_name='Автор'
     )
     review_id = models.ForeignKey(
-        Review, on_delete=models.CASCADE, related_name='comments'
+        Review, on_delete=models.CASCADE,
+        related_name='comments', verbose_name='Ревью'
     )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
