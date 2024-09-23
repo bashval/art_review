e323819d6e4e63996fee17c4952c5aa5bfaed601
Sanzhar <96529252+S4nzh4r@users.noreply.github.com>
2024-09-23 20:23:03 +0500

    Delete extra viewsets import

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,7 +2,6 @@ from django.db.models import Avg
 
 from django_filters.rest_framework import DjangoFilterBackend
 
-from rest_framework import viewsets
 from rest_framework import filters, viewsets
 from rest_framework.pagination import PageNumberPagination
 
