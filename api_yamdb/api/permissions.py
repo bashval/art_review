64ee8f86797b95e26a69ed24c0adf2ba5a836ee7
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-09-26 10:22:13 +0200

    Add properties to User Model and update permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,34 +7,26 @@ User = get_user_model()
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated
-                and (request.user.role == User.ADMIN
-                     or request.user.is_superuser))
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_authenticated
+                    and (request.user.is_admin or request.user.is_superuser)))
 
 
 class IsOwnerOrStaffOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return bool(
-            request.method in permissions.SAFE_METHODS
-            or (request.user.is_authenticated
-                and (request.user == obj.author
-                     or request.user.role == User.ADMIN
-                     or request.user.role == User.MODERATOR
-                     or request.user.is_superuser))
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_authenticated
+                    and (request.user == obj.author
+                         or request.user.is_admin
+                         or request.user.is_moderator
+                         or request.user.is_superuser)))
 
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user.is_authenticated
-                    and (request.user.role == User.ADMIN
-                         or request.user.is_superuser))
+        return (request.user.is_authenticated
+                and (request.user.is_admin or request.user.is_superuser))
