2e64412dbe02b3d6f2fe6a7b761bc33aa6fe6d8d
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-09-26 16:35:00 +0200

    Correct function name

--- api_yamdb/users/utils.py
+++ api_yamdb/users/utils.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 from api_yamdb.settings import CONFIRMATION_EMAIL
 
 
-def send_confirmatio_mail(email, code):
+def send_confirmation_mail(email, code):
     send_mail(
         subject='Confirmation Code',
         message=code,
